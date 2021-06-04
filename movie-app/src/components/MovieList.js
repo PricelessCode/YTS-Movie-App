242804e28e2ffd8fda464e9f76e3a7e1f5c2a9e9
Seungho Lee <poream3387@gmail.com>
2021-06-05 00:35:00 +0900

    Add Cover image

--- movie-app/src/components/MovieList.js
+++ movie-app/src/components/MovieList.js
@@ -4,13 +4,21 @@ import "./MovieList.scss";
 class MovieList extends React.Component {
     state = {
         movies: this.props.moviesFromParent,
+        clicked: false,
+        clickedImage: 0,
     }
 
     componentDidUpdate = (previousProps, previousState) => {
         if (this.state.movies != this.props.moviesFromParent) {
             this.setState({movies: this.props.moviesFromParent});
-            console.log(this.state.movies);
+            // console.log(this.state.movies);
         }
+    }
+
+    imgOnClick = (e) => {
+        // this.setState({clicked: true, clickedImage})
+        console.log('Clicked!!', e.target.getAttribute('data-key'));
+        this.setState({clicked: true, clickedImage: e.target.getAttribute('data-key')})
     }
     
     render() {
@@ -30,7 +38,7 @@ class MovieList extends React.Component {
         // }
 
         for(let i = 0; i < 5; i++) {
-            movieImages.push(<img key={i} class="" style={{width: '20%'}} src={tmp[i]} alt='yts movie snapshot'></img>);
+            movieImages.push(<img key={i} data-key={i} class="" style={{width: '20%'}} onClick={this.imgOnClick} src={tmp[i]} alt='yts movie snapshot'></img>);
         }
         return (
             <div>
@@ -39,6 +47,9 @@ class MovieList extends React.Component {
                         {movieImages}
                     </div>
                     
+                    <div id='coverImgDiv'>
+                    
+                    </div>
                 </div>
 
             </div>
